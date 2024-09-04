8d3a2f013beca6b2fdfd195a3fb24e5fa01e409c
Davronbek98 <yoldashevdavron1998@gmail.com>
2024-09-04 18:23:46 +0900

    feat: Task -G was completed

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,6 @@
+// Architectural pattern: MVC, Dependency Injection, MVP
 
+// MVC = MODEL VIEW CONTROLLER
+
+// Desin pattern: Middleware, Decorate
+console.log("I am here");
---
+++ src/train.ts
@@ -0,0 +1,18 @@
+// Task -G
+
+function getHighestIndex(arr: number[]) {
+  let maxIndex = 0;
+  let maxValue = arr[0];
+
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] > maxValue) {
+      maxValue = arr[i];
+      maxIndex = i;
+    }
+  }
+
+  return maxIndex;
+}
+
+const result = getHighestIndex([5, 21, 12, 21, 8]);
+console.log(result);
