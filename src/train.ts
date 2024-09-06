fbc942130eb99e28ad10370fe255680e99471b12
Davronbek98 <yoldashevdavron1998@gmail.com>
2024-09-06 17:59:47 +0900

    feat: TASK -H was completed

--- src/train.ts
+++ src/train.ts
@@ -1,18 +1,26 @@
 // Task -G
 
-function getHighestIndex(arr: number[]) {
-  let maxIndex = 0;
-  let maxValue = arr[0];
+// function getHighestIndex(arr: number[]) {
+//   let maxIndex = 0;
+//   let maxValue = arr[0];
 
-  for (let i = 1; i < arr.length; i++) {
-    if (arr[i] > maxValue) {
-      maxValue = arr[i];
-      maxIndex = i;
-    }
-  }
+//   for (let i = 1; i < arr.length; i++) {
+//     if (arr[i] > maxValue) {
+//       maxValue = arr[i];
+//       maxIndex = i;
+//     }
+//   }
 
-  return maxIndex;
+//   return maxIndex;
+// }
+
+// const result = getHighestIndex([5, 21, 12, 21, 8]);
+// console.log(result);
+
+// Task -H
+
+function getPositive(arr: number[]): string {
+  return arr.filter((num) => num > 0).join("");
 }
 
-const result = getHighestIndex([5, 21, 12, 21, 8]);
-console.log(result);
+console.log(getPositive([1, -4, 2]));
