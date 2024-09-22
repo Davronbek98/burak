53e2423b07ec0360e3756c13c12bcc8312a2e1fd
Davronbek98 <yoldashevdavron1998@gmail.com>
2024-09-22 21:59:57 +0900

    feat: locals variable integration

--- src/router-admin.ts
+++ src/router-admin.ts
@@ -16,8 +16,20 @@ routerAdmin.get("/logout", restaurantController.logout);
 routerAdmin.get("/check-me", restaurantController.checkAuthSession);
 
 /**Product */
-routerAdmin.get("/product/all", productController.getAllProduct);
-routerAdmin.post("/product/create", productController.creatNewProduct);
-routerAdmin.post("/product/:id", productController.updateChosenProduct);
+routerAdmin.get(
+  "/product/all",
+  restaurantController.verifyRestaurant,
+  productController.getAllProduct
+);
+routerAdmin.post(
+  "/product/create",
+  restaurantController.verifyRestaurant,
+  productController.creatNewProduct
+);
+routerAdmin.post(
+  "/product/:id",
+  restaurantController.verifyRestaurant,
+  productController.updateChosenProduct
+);
 /**User */
 export default routerAdmin;
