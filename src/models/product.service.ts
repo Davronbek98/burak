0c8131b633da25056f12f69495b0ef14008707f1
Davronbek98 <yoldashevdavron1998@gmail.com>
2024-09-23 13:56:17 +0900

    feat: configure product Schema model

--- src/models/product.service.ts
+++ src/models/product.service.ts
@@ -1,3 +1,11 @@
-class ProductService {}
+import ProductModel from "../schema/Product.model";
+
+class ProductService {
+  private readonly productmodel;
+
+  constructor() {
+    this.productmodel = ProductModel;
+  }
+}
 
 export default ProductService;
