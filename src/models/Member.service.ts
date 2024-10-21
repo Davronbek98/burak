9c6bde1011588a2b0ae37c059e8ab7644681bdb3
Davronbek98 <yoldashevdavron1998@gmail.com>
2024-10-21 14:12:42 +0900

    feat: develop getTopUsers rest Api

--- src/models/Member.service.ts
+++ src/models/Member.service.ts
@@ -44,6 +44,20 @@ class MemberService {
     return result;
   }
 
+  public async getTopUsers(): Promise<Member[]> {
+    const result = await this.memberModel
+      .find({
+        member: MemberStatus.ACTIVE,
+        memberPoints: { $gte: 1 },
+      })
+      .sort({ memeberPoints: -1 })
+      .limit(4)
+      .exec();
+    if (!result) throw new Errors(HttpCode.NOT_FOUND, Message.NO_DATA_FOUND);
+
+    return result;
+  }
+
   /**SPA */
 
   public async signup(input: MemberInput): Promise<Member> {
