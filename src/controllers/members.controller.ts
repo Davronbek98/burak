216b8feaae5fa764190774ea761f82a57dcf4f73
Davronbek98 <yoldashevdavron1998@gmail.com>
2024-10-21 12:43:38 +0900

    feat: develop updateMember business logic

--- src/controllers/members.controller.ts
+++ src/controllers/members.controller.ts
@@ -6,6 +6,7 @@ import {
   MemberInput,
   LoginInput,
   ExtendedRequest,
+  MemberUpdateInput,
 } from "../libs/types/member";
 import Errors, { HttpCode, Message } from "../libs/Error";
 import AuthService from "../models/Auth.service";
@@ -81,6 +82,20 @@ memberController.getMemberDetail = (req: ExtendedRequest, res: Response) => {
     res.status(HttpCode.OK).json(result);
   } catch (err) {
     console.log("Error,getMemberDetail:", err);
+    if (err instanceof Errors) res.status(err.code).json(err);
+    else res.status(Errors.standart.code).json(Errors.standart);
+  }
+};
+memberController.updateMember = async (req: ExtendedRequest, res: Response) => {
+  try {
+    console.log("updateMember");
+    const input: MemberUpdateInput = req.body;
+    if (req.file) input.memberImage = req.file.path.replace(/\\/, "/");
+    const result = await memberService.updateMember(req.member, input);
+
+    res.status(HttpCode.OK).json(result);
+  } catch (err) {
+    console.log("Error,updateMember:", err);
     if (err instanceof Errors) res.status(err.code).json(err);
     else res.status(Errors.standart.code).json(Errors.standart);
   }
