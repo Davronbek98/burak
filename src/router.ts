1f4012cda2e9cc8c0676bbf39a44c7782b19f359
Davronbek98 <yoldashevdavron1998@gmail.com>
2024-10-21 14:51:49 +0900

    feat: create getRestaurant rest Api

--- src/router.ts
+++ src/router.ts
@@ -4,6 +4,7 @@ import memberController from "./controllers/members.controller";
 import uploader from "./libs/untlls/uploader";
 
 /** Member **/
+router.get("/member/restaurant", memberController.getRestaurant);
 router.post("/member/login", memberController.login);
 router.post("/member/signup", memberController.signup);
 router.post(
