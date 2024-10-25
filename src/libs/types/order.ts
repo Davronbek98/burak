a06cd8120facd84eb5d918f1439ac2df2b4079ca
Davronbek98 <yoldashevdavron1998@gmail.com>
2024-10-25 12:41:07 +0900

    feat: develop getMyOrders rest Api

--- src/libs/types/order.ts
+++ src/libs/types/order.ts
@@ -18,6 +18,9 @@ export interface Order {
   memberId: ObjectId;
   createdAt: Date;
   updatedAt: Date;
+  /** from aggregations **/
+  orderItem: OrderItem[];
+  productData: Product[];
 }
 
 export interface OrderItemInput {
@@ -25,4 +28,10 @@ export interface OrderItemInput {
   itemPrice: number;
   productId: ObjectId;
   orderId?: ObjectId;
+}
+
+export interface OrderInquiry {
+  page: number;
+  limit: number;
+  orderStatus: OrderStatus;
 }
