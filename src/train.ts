b5a16acccdc456b5ee3e9aa0e634829d8d656b78
Davronbek98 <yoldashevdavron1998@gmail.com>
2024-11-01 11:42:48 +0900

    feat: TASK-ZB was completed

--- src/train.ts
+++ src/train.ts
@@ -348,11 +348,19 @@ Database validation
 
 // TASK- ZA
 
-type Person = { age: number };
+// type Person = { age: number };
 
-function sortByAge(arr: Person[]): Person[] {
-  return arr.sort((a, b) => a.age - b.age);
+// function sortByAge(arr: Person[]): Person[] {
+//   return arr.sort((a, b) => a.age - b.age);
+// }
+
+// const result = sortByAge([{ age: 23 }, { age: 21 }, { age: 13 }]);
+// console.log(result);
+
+// TASK - ZB
+function randomBetween(min: number, max: number): number {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-const result = sortByAge([{ age: 23 }, { age: 21 }, { age: 13 }]);
+const result = randomBetween(30, 50);
 console.log(result);
